2d7029b8c21de91a5c3b71dc3673400da8b75682
Iñaki Urdaniz <iurdaniz66@gmail.com>
2026-02-06 00:43:14 -0300

    cart, cart_items, order, order_items schemas

--- database/policies.sql
+++ database/policies.sql
@@ -30,6 +30,22 @@ ON products
 FOR DELETE
 USING (auth.uid() = user_id);
 
+ALTER TABLE user_favorites ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY "User can view own favorites"
+ON user_favorites
+FOR SELECT
+USING (auth.uid() = user_id);
+
+CREATE POLICY "User can add own favorites"
+ON user_favorites
+FOR INSERT
+WITH CHECK (auth.uid() = user_id);
+
+CREATE POLICY "User can remove own favorites"
+ON user_favorites
+FOR DELETE
+USING (auth.uid() = user_id);
 
 ALTER TABLE variants ENABLE ROW LEVEL SECURITY;
 
@@ -102,3 +118,119 @@ SELECT
   (stock > 0) AS in_stock
 FROM variants
 WHERE active = true;
+
+ALTER TABLE carts ENABLE ROW LEVEL SECURITY;
+
+/* SELECT */
+CREATE POLICY "User can read own carts"
+ON carts
+FOR SELECT
+USING (auth.uid() = user_id);
+
+/* INSERT */
+CREATE POLICY "User can create cart"
+ON carts
+FOR INSERT
+WITH CHECK (auth.uid() = user_id);
+
+/* UPDATE */
+CREATE POLICY "User can update own cart"
+ON carts
+FOR UPDATE
+USING (auth.uid() = user_id);
+
+/* DELETE */
+CREATE POLICY "User can delete own cart"
+ON carts
+FOR DELETE
+USING (auth.uid() = user_id);
+
+ALTER TABLE cart_items ENABLE ROW LEVEL SECURITY;
+
+/* SELECT */
+CREATE POLICY "User can read own cart items"
+ON cart_items
+FOR SELECT
+USING (
+  auth.uid() IN (
+    SELECT user_id
+    FROM carts
+    WHERE carts.id = cart_items.cart_id
+  )
+);
+
+/* INSERT */
+CREATE POLICY "User can add items to own cart"
+ON cart_items
+FOR INSERT
+WITH CHECK (
+  auth.uid() IN (
+    SELECT user_id
+    FROM carts
+    WHERE carts.id = cart_items.cart_id
+  )
+);
+
+/* UPDATE */
+CREATE POLICY "User can update own cart items"
+ON cart_items
+FOR UPDATE
+USING (
+  auth.uid() IN (
+    SELECT user_id
+    FROM carts
+    WHERE carts.id = cart_items.cart_id
+  )
+);
+
+/* DELETE */
+CREATE POLICY "User can delete own cart items"
+ON cart_items
+FOR DELETE
+USING (
+  auth.uid() IN (
+    SELECT user_id
+    FROM carts
+    WHERE carts.id = cart_items.cart_id
+  )
+);
+
+ALTER TABLE orders ENABLE ROW LEVEL SECURITY;
+
+/* SELECT */
+CREATE POLICY "User can read own orders"
+ON orders
+FOR SELECT
+USING (auth.uid() = user_id);
+
+/* INSERT */
+CREATE POLICY "User can create own order"
+ON orders
+FOR INSERT
+WITH CHECK (auth.uid() = user_id);
+
+ALTER TABLE order_items ENABLE ROW LEVEL SECURITY;
+
+/* SELECT */
+CREATE POLICY "User can read own order items"
+ON order_items
+FOR SELECT
+USING (
+  auth.uid() IN (
+    SELECT user_id
+    FROM orders
+    WHERE orders.id = order_items.order_id
+  )
+);
+
+/* INSERT */
+CREATE POLICY "User can insert order items"
+ON order_items
+FOR INSERT
+WITH CHECK (
+  auth.uid() IN (
+    SELECT user_id
+    FROM orders
+    WHERE orders.id = order_items.order_id
+  )
+);
--- database/schema.sql
+++ database/schema.sql
@@ -1,7 +1,29 @@
+-- Tipos ENUM
+CREATE TYPE fabric_type AS ENUM ('Cotton', 'Polyester');
+
+CREATE TYPE size_type AS ENUM ('XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL');
+
+CREATE TYPE order_status_type AS ENUM ('pending', 'processing', 'shipped', 'delivered', 'cancelled');
+
+CREATE TYPE cart_status_type AS ENUM ('active', 'completed', 'abandoned');
+
+CREATE TYPE gender_type AS ENUM ('male', 'female', 'other');
+
+CREATE TYPE role_type AS ENUM ('admin', 'user');
+
 CREATE TABLE users (
     id uuid PRIMARY KEY REFERENCES auth.users(id) ON DELETE CASCADE,
     email text NOT NULL UNIQUE,
-    role text NOT NULL DEFAULT 'user',
+    password_hash text NOT NULL,
+    role role_type NOT NULL DEFAULT 'user',
+    gender gender_type NOT NULL,
+    first_name text NOT NULL,
+    last_name text NOT NULL,
+    phone text NOT NULL,
+    address text NOT NULL,
+    city text NOT NULL,
+    country text NOT NULL,
+    birth_date date NOT NULL,
     created_at timestamptz DEFAULT NOW(),
     updated_at timestamptz DEFAULT NOW()
 );
@@ -11,17 +33,25 @@ CREATE TABLE products (
     user_id uuid NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     name text NOT NULL,
     description text,
-    fabric text,
+    fabric fabric_type NOT NULL,
     care_instructions text,
     active boolean DEFAULT true,
     created_at timestamptz DEFAULT NOW(),
     updated_at timestamptz DEFAULT NOW()
 );
 
+CREATE TABLE user_favorites (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id uuid NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    product_id uuid NOT NULL REFERENCES products(id) ON DELETE CASCADE,
+    created_at timestamptz DEFAULT NOW(),
+    UNIQUE (user_id, product_id)
+);
+
 CREATE TABLE variants (
     id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     product_id uuid NOT NULL REFERENCES products(id) ON DELETE CASCADE,
-    size text NOT NULL CHECK (size IN ('XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL')),
+    size size_type NOT NULL,
     color text NOT NULL,
     stock integer NOT NULL DEFAULT 0,
     price numeric(10,2) NOT NULL,
@@ -33,6 +63,45 @@ CREATE TABLE variants (
     updated_at timestamptz DEFAULT NOW(),
     UNIQUE (product_id, size, color)
 );
+
+CREATE TABLE carts (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id uuid NOT NULL REFERENCES auth.users(id) ON DELETE CASCADE,
+    status cart_status_type DEFAULT 'active',
+    created_at timestamptz DEFAULT NOW(),
+    updated_at timestamptz DEFAULT NOW(),
+    UNIQUE (user_id, status)
+);
+
+CREATE TABLE cart_items (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    cart_id uuid NOT NULL REFERENCES carts(id) ON DELETE CASCADE,
+    variant_id uuid NOT NULL REFERENCES variants(id) ON DELETE CASCADE,
+    quantity integer NOT NULL DEFAULT 1 CHECK (quantity > 0),
+    created_at timestamptz DEFAULT NOW(),
+    updated_at timestamptz DEFAULT NOW()
+);
+
+CREATE TABLE orders (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id uuid NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    total_amount numeric(10,2) NOT NULL,
+    status order_status_type DEFAULT 'pending',
+    created_at timestamptz DEFAULT NOW(),
+    updated_at timestamptz DEFAULT NOW(),
+);
+
+CREATE TABLE order_items (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    order_id uuid NOT NULL REFERENCES orders(id) ON DELETE CASCADE,
+    variant_id uuid NOT NULL REFERENCES variants(id),
+    quantity integer NOT NULL DEFAULT 1 CHECK (quantity > 0),
+    price numeric(10,2) NOT NULL,
+    created_at timestamptz DEFAULT NOW(),
+    UNIQUE (order_id, variant_id)
+);
+
+
 
 -- Triggers para actualizar updated_at
 CREATE OR REPLACE FUNCTION set_updated_at()
